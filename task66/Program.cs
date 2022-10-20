bfb697da58144c3ec8f6c7730a44e02b77441c07
Сергей <sergemartin@yandex.ru>
2022-10-20 23:21:53 +0300

    Решена задача 66

---
+++ task66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №66
+Console.Clear();
+string Pechat(int n, int m)
+{
+    if (n == m)
+    {
+        return $"{m}";
+    }
+    return Pechat(n, m - 1) + $" {m} ";
+}
+
+
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(Pechat(n, m));
